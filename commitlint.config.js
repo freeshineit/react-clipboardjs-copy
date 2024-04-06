6e80ec89cbaca70911a7c3a60c689d7a961d9fc8
ShineShao <xiaoshaoqq@gmail.com>
2024-04-07 01:27:30 +0800

    version: 2.0.1-alpha.0

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,19 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'enhance', 'chore', 'test', 'doc', 'refactor', 'style', 'revert', 'ci'],
+      [
+        'feat',
+        'fix',
+        'enhance',
+        'chore',
+        'test',
+        'doc',
+        'refactor',
+        'style',
+        'revert',
+        'ci',
+        'version',
+      ],
     ],
   },
 };
