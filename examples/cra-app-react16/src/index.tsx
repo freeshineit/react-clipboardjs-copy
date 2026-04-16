18280d71fe80b810f8f795bc7491b53faa4a2c7c
ShineShao <xiaoshaoqq@gmail.com>
2026-04-17 00:32:39 +0800

    feat: update example

--- examples/cra-app-react16/src/index.tsx
+++ examples/cra-app-react16/src/index.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import { render } from "react-dom";
 import Page from "./Page";
 
-const container = document.getElementById("app");
+const container = document.getElementById("root");
 render(<Page />, container);
